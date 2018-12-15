4abbd1c8dfa912e1c0da595149190cf522d0b6c0
marcenp <marcenp23@gmail.com>
2018-12-15 19:29:26 -0300

    Arreglo de error en el sdk de para crear el orm

--- src/app/shared/sdk/services/core/error.service.ts
+++ src/app/shared/sdk/services/core/error.service.ts
@@ -1,14 +1,17 @@
 /* tslint:disable */
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, /*throwError*/ } from 'rxjs';
+//import { Observable, throwError } from 'rxjs';
+import { Observable} from 'rxjs';
+import {_throw} from 'rxjs/observable/throw';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 /**
  * Default error handler
  */
 @Injectable()
 export class ErrorHandler {
-  public handleError(errorResponse: HttpErrorResponse): Observable<never> {
-    // return throwError(errorResponse.error.error || 'Server error');
-    return null;
+  public handleError(errorResponse: HttpErrorResponse): ErrorObservable{//Observable<never> {
+   // return throwError(errorResponse.error.error || 'Server error');
+   return _throw(errorResponse.error.error || 'Server error');
   }
 }
